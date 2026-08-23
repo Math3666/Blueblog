0d6c0ef373e572e2b974b61e38f69c1915b375d9
Math3666 <3478695072@qq.com>
2026-08-24 03:38:16 +0800

    Auto sync source code for Vercel 🚀

--- siteConfig.ts
+++ siteConfig.ts
@@ -29,7 +29,7 @@ export const siteConfig = {
 
   // 5. 首页照片墙预览图
   photoWallImage: "https://bu.dusays.com/2026/03/24/69c1e38b4c370.jpg",
-  cloudMusicIds: ["1809646618", "3361076230", "1859390262"],
+  cloudMusicIds: ["37955047", "3414335451", "3408617878", "1867150097"],
   social: {
     github: "https://github.com/Math3666",
     gitee: "",
